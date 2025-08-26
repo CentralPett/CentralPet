ca462b9117edc743c393000c4c00e3eabf7702c4
Alberth Nery <iophyhd@gmail.com>
2025-08-26 05:10:49 -0300

    feat: added alert function in botao-form (index.html)

--- JS/Principal.js
+++ JS/Principal.js
@@ -1,77 +1,79 @@
-const container = document.getElementById('adocao_pets');
+const container = document.getElementById("adocao_pets");
 const scrollAmount = 460;
 const intervalTime = 3000;
 
 let scrollPosition = 0;
 let maxScrollLeft = container.scrollWidth - container.clientWidth;
 
-function autoScroll(){
-    scrollPosition += scrollAmount;
+function autoScroll() {
+  scrollPosition += scrollAmount;
 
-    if(scrollPosition > maxScrollLeft){
-        scrollPosition = 0;
-    }
+  if (scrollPosition > maxScrollLeft) {
+    scrollPosition = 0;
+  }
 
-    container.scrollTo({
-        left: scrollPosition,
-        behavior: 'smooth'
-    });
-
+  container.scrollTo({
+    left: scrollPosition,
+    behavior: "smooth",
+  });
 }
 
 setInterval(autoScroll, intervalTime);
 
-
-
-const container_card = document.querySelector('.carrosel_geral_container'); // CORRIGIDO
+const container_card = document.querySelector(".carrosel_geral_container"); // CORRIGIDO
 const intervalTime_card = 3000;
 
 let scrollPosition_card = 0;
-let maxScrollLeft_card = container_card.scrollWidth - container_card.clientWidth;
+let maxScrollLeft_card =
+  container_card.scrollWidth - container_card.clientWidth;
 
-function getScrollAmount(){
+function getScrollAmount() {
+  const card = container_card.querySelector(".carrosel_geral_item");
+  if (!card) return container_card.clientWidth;
 
-    const card = container_card.querySelector('.carrosel_geral_item');
-    if(!card) return container_card.clientWidth;
+  const style = getComputedStyle(card);
+  const marginRight = parseInt(style.marginRight) || 0;
+  const marginLeft = parseInt(style.marginLeft) || 0;
 
-    const style = getComputedStyle(card);
-    const marginRight = parseInt(style.marginRight) || 0;
-    const marginLeft = parseInt(style.marginLeft) || 0;
-
-    return card.offsetWidth + marginLeft + marginRight;
-
+  return card.offsetWidth + marginLeft + marginRight;
 }
 
-function autoScroll_card(){
-    maxScrollLeft_card = container_card.scrollWidth - container_card.clientWidth;
+function autoScroll_card() {
+  maxScrollLeft_card = container_card.scrollWidth - container_card.clientWidth;
 
-    scrollPosition_card += getScrollAmount();
+  scrollPosition_card += getScrollAmount();
 
-    if(scrollPosition_card > maxScrollLeft_card){
-        scrollPosition_card = 0;
-    }
+  if (scrollPosition_card > maxScrollLeft_card) {
+    scrollPosition_card = 0;
+  }
 
-    container_card.scrollTo({
-        left: scrollPosition_card,
-        behavior: 'smooth'
-    })
+  container_card.scrollTo({
+    left: scrollPosition_card,
+    behavior: "smooth",
+  });
 }
 
 const autoScrollInterval = setInterval(autoScroll_card, intervalTime_card);
 
-const btnLeft = document.getElementById('left');
-const btnRight = document.getElementById('right');
+const btnLeft = document.getElementById("left");
+const btnRight = document.getElementById("right");
 
-btnLeft.addEventListener('click', () => {
-    container_card.scrollBy({
-        left: -getScrollAmount(),
-        behavior: 'smooth'
-    });
+btnLeft.addEventListener("click", () => {
+  container_card.scrollBy({
+    left: -getScrollAmount(),
+    behavior: "smooth",
+  });
 });
 
-btnRight.addEventListener('click', () => {
-    container_card.scrollBy({
-        left: getScrollAmount(),
-        behavior: 'smooth'
-    });
+btnRight.addEventListener("click", () => {
+  container_card.scrollBy({
+    left: getScrollAmount(),
+    behavior: "smooth",
+  });
+});
+
+const botao = document.getElementById("botao-form");
+
+botao.addEventListener("click", function () {
+  alert("Sua mensagem foi enviada!");
 });
